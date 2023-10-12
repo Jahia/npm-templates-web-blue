f873b87a3eb61228fe3905dd1006badd8e0fa42b
hduchesne <hduchesne@jahia.com>
2023-10-12 19:55:42 +0200

    start to rebuild the site with updated helpers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import {registry} from '@jahia/server-helpers';
+// import repeat from 'handlebars-helper-repeat';
+const repeat = require('handlebars-helper-repeat');
 
+const Handlebars = registry.get('module', 'handlebars');
+Handlebars.exports.registerHelper('repeat', repeat);
