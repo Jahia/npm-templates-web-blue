9b4ade73f97359eae136a0445424f5770826b69d
hduchesne <hduchesne@jahia.com>
2023-10-13 18:30:52 +0200

    continue to rebuild the site with updated helpers

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@ const repeat = require('handlebars-helper-repeat');
 
 const Handlebars = registry.get('module', 'handlebars');
 Handlebars.exports.registerHelper('repeat', repeat);
+
+Handlebars.exports.registerHelper('area', ({name, type, index}) =>{
+    const area = {
+        name: name + index,
+        nodeType: type ? type : "jnt:area"
+    }
+
+    console.log("**** area :",JSON.stringify(area));
+    return area;
+
+});
+
+Handlebars.exports.registerHelper('concat', (str, suffix) => {
+    return str + suffix;
+});
