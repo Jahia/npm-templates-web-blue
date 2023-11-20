a7b2eb43975e7f58d5f3e7aa64141624da908bc1
hduchesne <hduchesne@jahia.com>
2023-11-20 10:09:43 +0100

    BACKLOG-20960 : update css and comment unused code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-import {registry} from '@jahia/server-helpers';
-// Import repeat from 'handlebars-helper-repeat';
-const repeat = require('handlebars-helper-repeat');
+// import {registry} from '@jahia/server-helpers';
+// import repeat from 'handlebars-helper-repeat';
+// const repeat = require('handlebars-helper-repeat');
 
-const Handlebars = registry.get('module', 'handlebars');
-Handlebars.exports.registerHelper('repeat', repeat);
-
-Handlebars.exports.registerHelper('area', ({name, type, index}) => {
-    const area = {
-        name: name + index,
-        nodeType: type ? type : 'jnt:area'
-    };
-
-    console.log('**** area :', JSON.stringify(area));
-    return area;
-});
-
-Handlebars.exports.registerHelper('concat', (str, suffix) => {
-    return str + suffix;
-});
+// const Handlebars = registry.get('module', 'handlebars');
+// Handlebars.exports.registerHelper('repeat', repeat);
+//
+// Handlebars.exports.registerHelper('concat', (str, suffix) => {
+//     return str + suffix;
+// });
